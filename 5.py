d389c3ef198d56fd991415b4a2056ffd36a41102
vvb <vvb@cisco.com>
2017-02-26 21:17:46 +0530

    time optimization. now runs in sub second versus about 58s earlier
    Signed-off-by: vvb <vvb@cisco.com>

--- 5.py
+++ 5.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 
 divide_by = [11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
+sum = 2*3*5*7*11*13*17*19
 
 
 def is_divisible(num):
@@ -9,11 +10,12 @@ def is_divisible(num):
             return False
     return True
 
-
-num = 1
+# since the number is divisible by all number until 20, it has to be a multiple
+# of multipliers of all prime number until 20.
+num = sum
 while True:
     if is_divisible(num):
         break
-    num += 1
+    num += sum
 
 print(num)
